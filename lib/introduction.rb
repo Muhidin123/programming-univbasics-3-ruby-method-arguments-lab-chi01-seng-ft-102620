9937410114ca73f4c1472c2cedc259a3df531511
Muhidin Hukic <muhidinhukic1@gmail.com>
2020-09-15 19:09:39 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -11,7 +11,7 @@ end
 
 
 def introduction_with_language(name, language)
-  "Hi, my name is #{name} and I am learning to program in #{language}.\n"
+  puts"Hi, my name is #{name} and I am learning to program in #{language}.\n"
 end
 
 
